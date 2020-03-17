b59d8e1dce35bf39a3421bad4e6f03cc8a3ced71
Boulevard <30576543+BeachBoulevard@users.noreply.github.com>
2020-03-17 13:16:39 +0100

    search filters followed by q objects

--- books/views.py
+++ books/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth.mixins import (
   LoginRequiredMixin,
   PermissionRequiredMixin
 )
+from django.db.models import Q
 # Create your views here.
 from django.views.generic import ListView, DetailView
 
@@ -21,3 +22,13 @@ class BookDetailView(LoginRequiredMixin, PermissionRequiredMixin, DetailView):
   login_url = 'account_login'
   permission_required = 'books.special_status'
   
+class SearchResultsListView(ListView):
+  model = Book
+  context_object_name = 'book_list'
+  template_name = 'books/search_results.html'
+
+  def get_queryset(self):
+    query= self.request.GET.get('q')
+    return Book.objects.filter(
+        Q(title__icontains=query) | Q(title__icontains=query)
+    )
