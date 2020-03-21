04d5e60eae825380193711272793216d8efb9c16
Boulevard <30576543+BeachBoulevard@users.noreply.github.com>
2020-03-21 19:40:19 +0100

    secure_proxy_ssl_header and allowed hosts update

--- bookstore_project/settings.py
+++ bookstore_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = int(os.environ.get('DEBUG', default=0))
 
-ALLOWED_HOSTS = ['.herokuapp', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['guarded-meadow-23348.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
@@ -205,6 +205,7 @@ if ENVIRONMENT == 'production':
     SECURE_CONTENT_TYPE_NOSNIFF = True
     SESSION_COOKIE_SECURE = True
     CSRF_COOKIE_SECURE = True
+    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 
     # Heroku
